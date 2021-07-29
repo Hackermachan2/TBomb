f433f0c142d4a0e8149a9dd169d332ea85041570
hackermachan <88127923+Hackermachan2@users.noreply.github.com>
2021-07-29 12:19:01 +0530

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -32,14 +32,14 @@ banner() {
         figlet TBomb
     fi
     if ! [ -x "$(command -v toilet)" ]; then
-        echo -e "\e[4;34m This Bomber Was Created By \e[1;32mSpeedX \e[0m"
+        echo -e "\e[4;34m This Bomber Was Created By \e[1;32mhackermachan \e[0m"
     else
         echo -e "\e[1;34mCreated By \e[1;34m"
-        toilet -f mono12 -F border SpeedX
+        toilet -f mono12 -F border hackermachan
     fi
     echo -e "\e[1;34m For Any Queries Join Me!!!\e[0m"
-    echo -e "\e[1;32m           Telegram: https://t.me/TBombChat \e[0m"
-    echo -e "\e[4;32m   YouTube: https://www.youtube.com/c/SpeedXTech \e[0m"
+    echo -e "\e[1;32m           Telegram: https://t.me/hackermachan \e[0m"
+    echo -e "\e[4;32m   YouTube:https://youtube.com/channel/UC5AmiT44HyEZ2Rkc0WT0ZZA \e[0m"
     echo " "
     echo "NOTE: Kindly move to the PIP version Of TBomb for more stability."
     echo " "
